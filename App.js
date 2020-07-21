0378fdb8e5cee1130828643c9b71cdd39bdbb3fb
Victor Longon <victor.longon@etraveligroup.com>
2020-07-21 17:00:31 +0200

    setup lint

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/HomeScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
 const Stack = createStackNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          options={{ title: "Welcome" }}
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        options={{ title: "Welcome" }}
+        component={HomeScreen}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-};
+const HomeScreen = () => (
+  <View>
+    <Text>HomeScreen</Text>
+  </View>
+);
 
 export default HomeScreen;
